4ab10f2483fe49023ec79a6c17058f2e0315d828
Mariusz Szultka <mariusz.szultka@gmail.com>
2023-10-26 11:21:01 +0200

    page.js CSS added

--- app/page.js
+++ app/page.js
@@ -3,7 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <h1 className="bg-yellow-800 text-white text-center">Hello world!!</h1>
+      <h1 className="bg-warning text-dark text-center border-solid border-dark border-8">
+        Hello world!!
+      </h1>
       <h2 className="text-yellow-800">Hello world two!</h2>
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        primary: "#0D6EFD",
+        warning: "#ffc107",
+        danger: "#dc3545",
+        success: "#198754",
+        dark: "#212529",
+        secondary: "#6c757d",
+        light: "#f8f9fa",
       },
     },
   },
   plugins: [],
-}
+};
