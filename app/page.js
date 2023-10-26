90d6b870b8b0fa78a60f953222b281a1bcd180f0
Mariusz Szultka <mariusz.szultka@gmail.com>
2023-10-26 11:22:32 +0200

    page test css change

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <h1 className="bg-warning text-dark text-center border-solid border-dark border-8">
+      <h1 className="bg-primary text-dark text-center border-solid border-dark border-8">
         Hello world!!
       </h1>
       <h2 className="text-yellow-800">Hello world two!</h2>
